784249cdbf13131056701a910e6760609d4ca9ee
Dimitri Philips <dimitriphilips@msn.com>
2017-06-22 12:59:37 +0200

    Android app start + imagebuttons

---
+++ Project4RS-swag.ga/app/src/main/java/ga/rs_swag/project4rs_swagga/MainActivity.java
@@ -0,0 +1,15 @@
+package ga.rs_swag.project4rs_swagga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Project4RS-swag.ga/app/src/test/java/ga/rs_swag/project4rs_swagga/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ga.rs_swag.project4rs_swagga;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
